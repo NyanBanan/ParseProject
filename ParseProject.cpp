2b523f14d19d72b2a7c56c0ff760ed13daaa0074
NyanBanan <mokshin2002nikita@gmail.com>
2022-07-14 18:22:11 +0300

    First exeptions and read config

--- ParseProject.cpp
+++ ParseProject.cpp
@@ -1,34 +1,61 @@
 
 #include "ParseProject.h"
 
-bool getConfig(std::string& file_for_conf){
-    std::ifstream conf("config.json", std::ifstream::binary);
-    if(!conf.is_open())
-        return false;
-    
-}
 
 
-bool connect(){
-    std::ifstream conf("parameters.json", std::ifstream::binary);
+void connect(){
+    std::ifstream conf("paramet312312ers.json", std::ifstream::binary);
+
     if(!conf.is_open())
-        return false;
+        throw std::exception("Config file not open");
+
     Poco::JSON::Parser parser;
     
     auto pParam=parser.parse(conf).extract<Poco::JSON::Object::Ptr>();
+    conf.close();
     std::string key=pParam->getValue<std::string>("key");
     std::string https=pParam->getValue<std::string>("https");
     if(key.empty()){
         std::cout<<"EMPTYYYYY"<<std::endl;
-
     }
-    std::cout<<key;
-    std::cout<<https;
+    std::cout<<key<<std::endl;
+    std::cout<<https<<std::endl;
 }
 
 int main(){
-    std::string fakeJson="{ \"key\" : \"\", \"https\":\"https://github.com/\"}";
-    connect();
+
+    Poco::AutoPtr<Poco::SimpleFileChannel> pChannel(new Poco::SimpleFileChannel);
+    pChannel->setProperty("path", "Work.log");
+    pChannel->setProperty("rotation", "2 K");  
+    pChannel->setProperty("flush", "true");
+
+   /*  Poco::AutoPtr<Poco::PatternFormatter> patternFormatter(
+        new Poco::PatternFormatter("[%Y-%m-%d  %H:%M:%s] [%U(%u)] %p: %t"));
+    patternFormatter->setProperty("times", "local");
+    Poco::AutoPtr<Poco::FormattingChannel> formattingChannel(
+       new Poco::FormattingChannel(patternFormatter, pChannel));
+
+    Poco::Logger::root().setChannel(formattingChannel); */
+    Poco::Logger::root().setChannel(pChannel);
+
+    Poco::Logger& logger = Poco::Logger::get("WorkLogger");
+
+    try
+    {
+        connect();
+        logger.information("Work correct");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        logger.error(e.what());
+    }
+
     system("pause");
+    pChannel->close();
     return 0;
 }
+
+/*  Poco::Path a;
+    std::cout<<Poco::Path::find("D:/Diff Folders From C/Workbench/Code/newRepos/ParseProject","parameters.json",a); 
+    std::cout<<a.toString();*/
--- ParseProject.h
+++ ParseProject.h
@@ -4,5 +4,10 @@
 #include <fstream>
 #include <Poco/JSON/Parser.h>
 #include <Poco/Dynamic/Var.h>
-
+#include <Poco/File.h>
+#include <Poco/Path.h>
+#include <Poco/SimpleFileChannel.h>
+#include <Poco/FormattingChannel.h>
+#include <Poco/Logger.h>
+#include <Poco/PatternFormatter.h>
 #endif
