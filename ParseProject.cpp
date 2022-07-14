68c088ff0452a4aa5bd92f7c165b45650b3a1bda
NyanBanan <mokshin2002nikita@gmail.com>
2022-07-14 15:13:54 +0300

    try parse config

--- ParseProject.cpp
+++ ParseProject.cpp
@@ -1,8 +1,34 @@
 
-#include "h/ParseProject.h"
+#include "ParseProject.h"
 
+bool getConfig(std::string& file_for_conf){
+    std::ifstream conf("config.json", std::ifstream::binary);
+    if(!conf.is_open())
+        return false;
+    
+}
+
+
+bool connect(){
+    std::ifstream conf("parameters.json", std::ifstream::binary);
+    if(!conf.is_open())
+        return false;
+    Poco::JSON::Parser parser;
+    
+    auto pParam=parser.parse(conf).extract<Poco::JSON::Object::Ptr>();
+    std::string key=pParam->getValue<std::string>("key");
+    std::string https=pParam->getValue<std::string>("https");
+    if(key.empty()){
+        std::cout<<"EMPTYYYYY"<<std::endl;
+
+    }
+    std::cout<<key;
+    std::cout<<https;
+}
 
 int main(){
-    Poco::JSON::Array a;
+    std::string fakeJson="{ \"key\" : \"\", \"https\":\"https://github.com/\"}";
+    connect();
+    system("pause");
     return 0;
 }
--- ParseProject.h
+++ ParseProject.h
@@ -1,3 +1,8 @@
-#pragma once
-#include <Poco/JSON/Parser.h>
+#ifndef PARSE_PROJ_INCLUDE
+#define PARSE_PROJ_INCLUDE
 #include <iostream>
+#include <fstream>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+
+#endif
